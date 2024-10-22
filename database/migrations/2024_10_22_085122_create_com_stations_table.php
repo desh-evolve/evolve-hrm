7f1131edae1ee25395b7810bc32f466e69db7fd5
Manori Indika <manori@evolve-sl.com>
2024-10-22 16:44:21 +0530

    Station

---
+++ database/migrations/2024_10_22_085122_create_com_stations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('com_stations', function (Blueprint $table) {
+            $table->id(); 
+            $table->unsignedBigInteger('branch_id'); 
+            $table->unsignedBigInteger('station_type_id'); 
+            $table->unsignedBigInteger('station_customer_id')->nullable(); 
+            $table->unsignedBigInteger('source')->nullable(); 
+            $table->string('description')->nullable(); 
+            $table->string('time_zone')->nullable(); 
+            
+            $table->string('status')->default('active')->nullable();
+            $table->timestamp('created_at')->useCurrent();
+            $table->integer('created_by')->nullable();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+            $table->integer('updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('com_stations');
+    }
+};
