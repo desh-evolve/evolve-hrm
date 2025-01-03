ff8354c39a6932752200491a0599cf2e46c11845
desh-evolve <deshan@evolve-sl.com>
2025-01-03 12:14:40 +0530

    20240103-1214-desh

--- routes/web.php
+++ routes/web.php
@@ -84,8 +84,8 @@ Route::group(['middleware' => ['role:super-admin|admin']], function () {
     Route::get('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'addPermissionToRole']);
     Route::put('roles/{roleId}/give-permissions', [App\Http\Controllers\RoleController::class, 'givePermissionToRole']);
 
-    Route::resource('users', App\Http\Controllers\employeeController::class);
-    Route::get('users/{userId}/delete', [App\Http\Controllers\employeeController::class, 'destroy']);
+    Route::resource('users', App\Http\Controllers\UserController::class);
+    Route::get('users/{userId}/delete', [App\Http\Controllers\UserController::class, 'destroy']);
 
     //==============================================================================================================================
     // Industry
